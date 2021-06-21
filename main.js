00fa4efb379464f61ce47e9ad1cf5426fb20e33e
AP10119 <75711027+AP10119@users.noreply.github.com>
2021-06-20 19:52:29 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,57 @@ var heightblock=30;
 var playerimage="";
 var blockimage="";
 function updatep(){
-    fabric.Image.fromURL("https://www.pinclipart.com/picdir/middle/546-5463411_transparent-thor-clipart-avengers-endgame-memes-thor-png.png",function(img){
+    fabric.Image.fromURL("player.png",function(img){
         playerimage=img;
         playerimage.scaleToWidth(40);
         playerimage.scaleToHeight(75);
         playerimage.set({top:playery_axis,left:playerx_axis});
         canvas10.add(playerimage)
     })
+}
+function updateb(blocks){
+    fabric.Image.fromURL(blocks,function(blockimg){
+        blockimage=blockimg;
+        blockimage.scaleToWidth(widthblock);
+        blockimage.scaleToHeight(heightblock);
+        blockimage.set({top:playery_axis,left:playerx_axis});
+        canvas10.add(blockimage);
+    })
+}
+window.addEventListener("keydown",movedown);
+function movedown(e){
+    var kcode=e.keyCode
+    if(kcode=="70"){
+        updatep();
+        updateb("thor_face.png");
+    }
+    if(kcode=="66"){
+        updatep();
+        updateb("spiderman_body.png");
+    }
+    if(kcode=="76"){
+        updateb("hulk_legs.png");
+        updatep();
+    }
+    if(kcode=="82"){
+        updateb("ironman_right_hand.png");
+        updatep();
+    }
+    if(kcode=="65"){
+        updateb("captain_america_left_hand.jpg");
+        updatep();
+    }
+    if(kcode=="77"&e.shiftKey){
+        widthblock=widthblock-5;
+        heightblock=heightblock-5;
+        document.getElementById("blockwidth").innerHTML=widthblock;
+        document.getElementById("blockheight").innerHTML=heightblock;
+    }
+    if(kcode=="80"&e.shiftKey){
+        widthblock=widthblock+5;
+        heightblock=heightblock+5;
+        document.getElementById("blockwidth").innerHTML=widthblock;
+        document.getElementById("blockheight").innerHTML=heightblock;
+    }
+
 }
